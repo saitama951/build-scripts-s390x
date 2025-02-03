dcb4221d3c82d078d0ec8890ff0e1b3a874a7d7e
saitama951 <sanjamkumar360@gmail.com>
2025-02-03 20:24:19 +0100

    Add docs on usage refactor script

--- runtime/README.md
+++ runtime/README.md
@@ -1,3 +1,7 @@
+Pre-req:
+* cd build-scripts-s390x/runtime
+* mkdir sdk && mkdir packages
+* install native dependencies (https://github.com/dotnet/runtime/blob/main/eng/common/native/install-dependencies.sh)
 USAGE:
 * sh automate-runtime.sh branch-name
 * default branch-name: main
--- runtime/automate-runtime.sh
+++ runtime/automate-runtime.sh
@@ -2,13 +2,13 @@ set -x
 
 BASEDIR=$(pwd)
 message=()
-version=""
+version=""1
 commit_id=""
 function clone_runtime {
 	git clone --branch ${1:-main} https://github.com/dotnet/runtime
 	pushd runtime
 	commit_id=$(git show HEAD --pretty=format:"%h" --no-patch)
-	message+=("commit id:- ${commit_id}")
+	message+=("commit id:- ${commit_id} <br>")
 	popd
 }
 
@@ -22,24 +22,23 @@ function extract_sdk {
 	if [ $? -ne 0 ]; then
 		pushd sdk
 		gh release download v${version} --repo IBM/dotnet-s390x --pattern dotnet-sdk-${version}-linux-s390x.tar.gz
-		popd
 		if [ $? -ne 0 ]; then
-			message+=("failed to get the dotnet runtime. please build the sdk with version ${version}")
-			return 1
+			message+=("failed to get the dotnet host SDK.<br>please build the sdk with version ${version}")
+			eval $(shoot_email)
+			exit 1
 		fi
+		popd
 		pushd packages
 		gh release download v$version --repo IBM/dotnet-s390x --pattern '*linux-s390x*.nupkg'
 		tar -xzf ${BASEDIR}/sdk/dotnet-sdk-$version-linux-s390x.tar.gz --directory ${BASEDIR}/runtime/.dotnet/
 		popd
 	fi
 }
-
 function build_runtime {
 	pushd runtime
 	runtime_build_flags=(
 		# llvm bug https://github.com/llvm/llvm-project/issues/109113
-		--cmakeargs -DCMAKE_CXX_COMPILER=\"/home/sanjam/llvm-project/build/bin/clang++\" 
-		--cmakeargs -DCMAKE_C_COMPILER=\"/home/sanjam/llvm-project/build/bin/clang\"
+		--cmakeargs -DCLR_CMAKE_USE_SYSTEM_ZLIB=true
 		--portablebuild false
 		--runtimeconfiguration Release 
 		--librariesConfiguration Debug 
@@ -59,24 +58,27 @@ function build_runtime {
 	./build.sh "${runtime_build_flags[@]}" > log
 
 	if [ $? -ne 0 ]; then
-		message+=("build failed! please check the errors")
+		message+=("build failed!<br>please check the errors")
 	else
 		message+=("build succeess!")
 	fi
-
 	popd
 }
 
 function shoot_email {
-	pushd runtime
+	#pushd runtime
 	recipents_emails=(
 	)
 	file=$(mktemp)
-	timestamp=i$(date)
+	timestamp=$(date)
 	echo "${message[@]}" > file
-	mutt -s "RUN ${timestamp}" -a "log" -- "${recipents_emails[@]}" < file
+	if [ $(find . -iname "log" | wc -l) -lt 1 ]; then
+		mutt -s "RUN ${timestamp}" -- "${recipents_emails[@]}" < file
+	else
+		mutt -s "RUN ${timestamp}" -a "log" -- "${recipents_emails[@]}" < file
+	fi
 	rm -rf file
-	popd	
+	#popd
 }
 
 run=(clone_runtime extract_sdk build_runtime shoot_email)
